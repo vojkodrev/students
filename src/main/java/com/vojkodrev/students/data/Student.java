02545fcf32b2577e01e4b04ccccd6f6af978e202
vojkodrev <vojkodrev@gmail.com>
2019-05-06 07:56:44 +0200

    Code formatting for student.

--- src/main/java/com/vojkodrev/students/data/Student.java
+++ src/main/java/com/vojkodrev/students/data/Student.java
@@ -6,31 +6,31 @@ import java.util.List;
 
 public class Student {
 
-	@Id
-	public String id;
+  @Id
+  public String id;
 
-	public String auth;
+  public String auth;
 
-	public String firstName;
-	public String lastName;
+  public String firstName;
+  public String lastName;
 
-	public List<String> enrollments;
+  public List<String> enrollments;
 
-	public Student() {}
+  public Student() {}
 
-	public Student(String auth, String firstName, String lastName, List<String> enrollments) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.auth = auth;
-		this.enrollments = enrollments;
-	}
+  public Student(String auth, String firstName, String lastName, List<String> enrollments) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.auth = auth;
+    this.enrollments = enrollments;
+  }
 
-	@Override
-	public String toString() {
-		return String.format(
-				"Student[id='%s', auth='%s', firstName='%s', lastName='%s', enrollments=%s]",
-				id, auth, firstName, lastName, enrollments);
-	}
+  @Override
+  public String toString() {
+    return String.format(
+      "Student[id='%s', auth='%s', firstName='%s', lastName='%s', enrollments=%s]",
+      id, auth, firstName, lastName, enrollments);
+  }
 
 }
 
